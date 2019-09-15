15e08053dcc6825db694deabf06b618b455429af
juliet-wambua <julienea289@gmail.com>
2019-09-15 23:28:36 +0300

    recylr view holders2

--- app/src/main/java/com/nea/myweather/adapters/WeatherAdapter.java
+++ app/src/main/java/com/nea/myweather/adapters/WeatherAdapter.java
@@ -1,51 +1,22 @@
 package com.nea.myweather.adapters;
 
 import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+
 import androidx.recyclerview.widget.RecyclerView;
-import com.bumptech.glide.Glide;
-import com.nea.myweather.R;
+
+import com.nea.myweather.models.Location;
+
+import java.util.List;
 
 public class WeatherAdapter {
-    List<LocationResponse> weatherDetails;
+    List<Location> weatherDetails;
     Context context;
 
-    public WeatherAdapter(List<LocationResponse> weatherDetails, Context context){
+    public WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>{
         this.weatherDetails=weatherDetails;
         this.context = context;
     }
-    @Override
-    public WeatherHolder onCreateViewHolder(ViewGroup parent, int viewType){
-        View view = LayoutInflater.from (context).inflate ( R.layout.weather_list,parent,false );
-        WeatherHolder weatherHolder = new WeatherHolder(view);
-        return  weatherHolder;
+
     }
 
-    @Override
-    public void onBindViewHolder(WeatherHolder holder, int position) {
-        holder.tvTitle.setText(weatherDetails.get(position).getObservationTime ());
-        holder.tvOverview.setText(weatherDetails.get(position).getTemperature ());
-        holder.tvReleaseDate.setText(weatherDetails.get(position).getWindSpeed ());
-        Glide.with(context).load("https://assets.weatherstack.com/images/wsymbols01_png_64/"+weatherDetails.get(position).getWeatherIcons ()).into(holder.ivMovie);
-    }
-    @Override
-    public int getItemCount() {
-        return weatherDetails.size();
-    }
-    public class WeatherHolder extends RecyclerView.ViewHolder {
-        TextView tvTitle,tvOverview,tvReleaseDate;
-        ImageView ivMovie;
-        public WeatherHolder(View v) {
-            super(v);
-            tvTitle = (TextView) v.findViewById(R.id.tvTitle);
-            tvOverview = (TextView) v.findViewById(R.id.tvOverView);
-            tvReleaseDate = (TextView) v.findViewById(R.id.tvReleaseDate);
-            ivMovie = (ImageView) v.findViewById(R.id.ivMovie);
-        }
-    }
-}
-}
+
