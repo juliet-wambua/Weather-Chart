6f11fe4b1b503190c5e9b992c6612c7c1cedd596
juliet-wambua <julienea289@gmail.com>
2019-09-16 09:45:08 +0300

    added a google-service.jason

--- app/src/main/java/com/nea/myweather/ui/WeatherListActivity.java
+++ app/src/main/java/com/nea/myweather/ui/WeatherListActivity.java
@@ -1,4 +1,27 @@
 package com.nea.myweather.ui;
 
-public class WeatherListActivity {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.nea.myweather.constants.Constants;
+
+public class WeatherListActivity extends AppCompatActivity {
+    private SharedPreferences mSharedPreferences;
+    private String mRecentAddress;
+//    ...
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+//    ...
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mRecentAddress = mSharedPreferences.getString( Constants.PREFERENCES_LOCATION_KEY, null);
+        Log.d("Shared Pref Location", mRecentAddress);
+    }
+//    ...
 }
